8fdb10b48a0296d545a8d1216edec6f87edebaef
wadimabr <wadimabr@gmail.com>
2021-12-09 00:08:08 +0100

    upd

--- main.js
+++ main.js
@@ -4,33 +4,30 @@ Array.prototype.random = function () {
 
 String.prototype.toCapitalized = function () {
     let first = this[0].toUpperCase();
-    let rest = this.slice(1)
-    return `${first}${rest}`
+    let rest = this.slice(1);
+    return `${first}${rest}`;
 }
 
 function initComponent(name, pics) {
     let picComponent = document.querySelector(`#${name}`);
     let button = picComponent.querySelector('button');
+    let text = picComponent.querySelector('p');
     let picContainer = picComponent.querySelector('div');
 
     button.onclick = function () {
-        let pic = pics.random()
-        picContainer.innerHTML = `<img src="resources/images/${name}/${pic}.jpg" alt="sample image"><p>${pic.toCapitalized()}</p>`
+        let pic = pics.random();
+        picContainer.innerHTML = `<img src="resources/images/${name}/${pic}.jpg" alt="sample image">`;
+        text.innerText = pic.toCapitalized();
     }
+    button.click();
 }
 
 let whoPics = ['monkey', 'naruto', 'pirate'];
+let whatPics = ['banana', 'mask', 'iphone'];
+let wherePics = ['castle', 'mountain', 'space'];
+let whenPics = ['morning', 'night', 'winter'];
 
-initComponent('who', whoPics)
-
-let whatPics = ['banana', 'mask', 'iphone']
-
-initComponent('what', whatPics)
-
-let wherePics = ['castle', 'mountain', 'space']
-
-initComponent('where', wherePics)
-
-let whenPics = ['morning', 'night', 'winter']
-
-initComponent('when', whenPics)
+initComponent('who', whoPics);
+initComponent('what', whatPics);
+initComponent('where', wherePics);
+initComponent('when', whenPics);
