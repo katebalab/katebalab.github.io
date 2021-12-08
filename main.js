f426772c30b8531c09f0d7d012e5594dfa503b34
wadimabr <wadimabr@gmail.com>
2021-12-08 23:11:17 +0100

    upd

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-let picContainer = document.querySelector('#test')
-
-function changePic() {
-    picContainer.innerHTML = '<img src="resources/images/images2.jpg" alt="sample image">'
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random() * this.length))];
 }
+
+function initWho() {
+    let pics = ['monkey', 'naruto', 'pirate'];
+    let picComponent = document.querySelector('#who');
+    let button = picComponent.querySelector('button');
+    let picContainer = picComponent.querySelector('div');
+
+    button.onclick = function () {
+        picContainer.innerHTML = `<img src="resources/images/who/${pics.random()}.jpg" alt="sample image">`
+    }
+}
+
+initWho()
